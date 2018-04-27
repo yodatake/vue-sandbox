e9c266a81aac0d33c21f28ea1c984a7d6145ae72
3140305 <K008028-MBP@jp1750049.local>
2018-04-27 16:54:30 +0900

    Initial

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,29 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <hello-world msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import { Component, Vue } from "vue-property-decorator";
+import HelloWorld from "./components/HelloWorld.vue";
+import InputText from "./components/nest/InputText.vue";
+import ListTable from "./components/nest/ListTable.vue";
 
 @Component({
   components: {
     HelloWorld,
-  },
+    InputText,
+    ListTable
+  }
 })
 export default class App extends Vue {}
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/input/InputText.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="input">
+    <input  v-model="txt" placeholder="edit me" />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue,  Model } from 'vue-property-decorator';
+
+@Component
+export default class InputText extends Vue {
+  @Model() private txt!: string;
+  update () : void{
+    console.info(this.txt);
+  }
+
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+input {
+  margin: 40px 0 0;
+  font: outline;
+  color: blue;
+  font-family: Arial, Helvetica, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+button {
+  width: 10px;
+}
+</style>
